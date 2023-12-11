b6e7ba12ae9be31880734e2d9e446ea69f7e7ed7
도건우 <87505243+doogunwo@users.noreply.github.com>
2023-12-12 02:30:53 +0900

    프로젝트에 파이어베이스.json 추가함 -> .gitignore 항목에 있기 때문에 커밋에 미포함됨

---
+++ app/src/main/java/com/cookandroid/moblieteamproject_gotobed/gunwo/dashboard.java
@@ -0,0 +1,14 @@
+package com.cookandroid.moblieteamproject_gotobed.gunwo;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.cookandroid.moblieteamproject_gotobed.R;
+
+public class dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+    }
+
+}
